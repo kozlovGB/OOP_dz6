572795d199d42c1c08fe31c38bc61d42d87eecdc
Георгий <hurricene001@mail.ru>
2022-12-12 15:36:13 +0300

    готово

---
+++ main.py
@@ -0,0 +1,22 @@
+print("два нуля в качестве знака операции"
+      "\nзавершит работу программы")
+while True:
+    s = input("Знак (+,-,*,/): ")
+    if s == '00':
+        break
+    if s in ('+', '-', '*', '/'):
+        x = float(input("x="))
+        y = float(input("y="))
+        if s == '+':
+            print("%.2f" % (x+y))
+        elif s == '-':
+            print("%.2f" % (x-y))
+        elif s == '*':
+            print("%.2f" % (x*y))
+        elif s == '/':
+            if y != 0:
+                print("%.2f" % (x/y))
+            else:
+                print("Деление на ноль!")
+    else:
+        print("Неверный знак операции!")
